f26f3c4281ba02936f7241c17ed34adf27be70d5
eduviana <eduardoviana83@gmail.com>
2023-02-15 20:13:22 -0300

    first commit

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,51 @@
+import "./navBar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+
+const NavBar = () => {
+  const { toggleDarkMode, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>SocialMedia</span>
+        </Link>
+        <HomeOutlinedIcon />
+        {darkMode ? (
+          <DarkModeOutlinedIcon onClick={toggleDarkMode} />
+        ) : (
+          <WbSunnyOutlinedIcon onClick={toggleDarkMode} />
+        )}
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src={currentUser.profilePic}
+            alt=""
+          />
+          <span>{currentUser.name}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
